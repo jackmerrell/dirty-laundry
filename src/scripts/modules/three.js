c06634225973a008f7adf3cbe71073d8f5262cf6
Jack Merrell <jackmerrell@gmail.com>
2020-07-11 23:45:14 +0200

    add depthmat

--- src/scripts/modules/three.js
+++ src/scripts/modules/three.js
@@ -1,13 +1,16 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const three = () => {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
-    75,
+    50,
     window.innerWidth / window.innerHeight,
     0.1,
     1000,
   );
+  camera.position.z = 0.4;
+  camera.position.y = 0.05;
 
   const renderer = new THREE.WebGLRenderer({
     antialias: true,
@@ -15,22 +18,27 @@ const three = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshNormalMaterial({
-    wireframe: true,
+  const material = new THREE.MeshDepthMaterial({
+    wireframe: false,
   });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
 
-  camera.position.z = 5;
+  const loader = new GLTFLoader();
+  loader.load('assets/images/scene.gltf', gltf => {
+    const model = gltf.scene;
+    model.rotation.z = 0.3;
+    model.scale.set(0.01, 0.01, 0.01);
+    model.traverse(child => {
+      child.material = material; // eslint-disable-line no-param-reassign
+    });
+    scene.add(model);
 
-  const animate = function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-  };
-  animate();
+    const animate = function animate() {
+      requestAnimationFrame(animate);
+      model.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    animate();
+  });
 
   window.onresize = () => {
     camera.aspect = window.innerWidth / window.innerHeight;
